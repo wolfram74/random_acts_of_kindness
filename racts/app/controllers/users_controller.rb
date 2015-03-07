f44d66fadc1f66744030d4705ee1033c6d804c78
wolfram74 <phaugen@gmail.com>
2015-03-07 11:35:54 -0800

    cleaned up controllers to only have necessary routes

--- racts/app/controllers/users_controller.rb
+++ racts/app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: @users
-  end
-
-  def new
-    @user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
   end
@@ -18,29 +9,4 @@ class UsersController < ApplicationController
     render json: @data
   end
 
-  def create
-    @user = User.create(user_params)
-    render json: @user
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id]).update(user_params)
-    render json: @user
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    render json: { user: User.all }
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password_hash, :username)
-  end
 end
