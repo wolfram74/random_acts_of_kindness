4a92594fd55a77bb39389a458de5f2b777fe22c5
wolfram74 <phaugen@gmail.com>
2015-03-06 20:59:18 -0800

    adjusted spec to accomodate new methods

--- racts/spec/models/user_spec.rb
+++ racts/spec/models/user_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe User, type: :model do
       args = {category_id: category_id, amount: amount, period: period}
       5.times{category.tasks << FactoryGirl.create(:task)}
       user.subscribe(args)
-      expect(user.active_tasks[1].length).to_not eq(0)
+      expect(user.active_tasks.length).to_not eq(0)
     end
   end
 
