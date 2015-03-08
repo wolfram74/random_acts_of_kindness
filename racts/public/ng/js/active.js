a2b94c364624680ad9c5ecb0ffb3b875a8a910c6
Shambhavi Sachid <shambu1985@gmail.com>
2015-03-07 19:54:47 -0800

    Working on completeTask

--- racts/public/ng/js/active.js
+++ racts/public/ng/js/active.js
@@ -8,9 +8,11 @@ racts.factory('activeTasksModel', [function() {
 
 
 
-racts.service('activeTasksResolver', ['$http', '$q', 'activeTasksModel', function($http, $q, activeTasksModel ) {
+racts.service('activeTasksResolver', ['$http', '$q', 'activeTasksModel', 'session', function($http, $q, activeTasksModel, session) {
 
-	console.log('fooo!!!')
+	console.log('TESTZONE weeee')
+	console.log( session.currentUser() )
+	console.log('TESTZONE weeee ENDDD')
 	var getActiveTasks = $http.get('http://localhost:3000/users/1/active')
 				.success(function(response) {
 					activeTasksModel.assignments = response
@@ -31,28 +33,41 @@ racts.service('activeTasksService', ['$http', '$q', 'activeTasksModel', function
 
 }])
 
-racts.service('activeTasksComplete', ['$http', '$q', 'activeTasksModel', 'activeTasksService', function($http, $scope, activeTasksModel, activeTasksService) {
-	var completedTask = function(task) {
-			if(confirm("Are you sure?")) {
-				$http.put("http://localhost:3000/assignments/" + #{task.assignemnt_id})
-				.success(function(response) {
-					console.log(response)
-
-				})
-				.error(function(response) {
-					console.log("Rejected")
-				})
-			}
-	}
-}])
+// racts.service('activeTasksComplete', ['$http', '$q', 'activeTasksModel', 'activeTasksService', function($http, $scope, activeTasksModel, activeTasksService) {
+// 	var completedTask = function(task) {
+// 			if(confirm("Are you sure?")) {
+// 				$http.put("http://localhost:3000/assignments/" + #{task.assignemnt_id})
+// 				.success(function(response) {
+// 					console.log(response)
+// 					var index =
+// 					activeTasksService.activeTasksModel.assignments.delete(task)
+// 				})
+// 				.error(function(response) {
+// 					console.log("Rejected")
+// 				})
+// 			}
+// 	}
+// }])
 
 racts.controller('activeTasksController', ['$http', '$scope', 'activeTasksService', function($http, $scope, activeTasksService){
 
 	$scope.assignments = activeTasksService.activeTasksModel.assignments
 
 	console.log($scope.assignments)
-
-	// $scope.completeTask = activeTasksComplete
+	// console.log( #{task.assignemnt_id})
+	$scope.completeTask =  function(task, index) {
+			if(confirm("Are you sure?")) {
+				 console.log("hello")
+				 console.log("http://localhost:3000/assignments/" + task.assignment_id)
+				$http.put("http://localhost:3000/assignments/" + task.assignment_id)
+				.success(function(response) {
+					console.log(response)
+					// $scope.assignments.splice(index,1);
+		    })
+				// .error(function(response) {
+				//   console.log("Rejected")
+			}
+	}
 }])
 
 
