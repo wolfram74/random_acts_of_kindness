b65e9dbdd29606e91d48530ea1b1f3225bd2eb1b
wolfram74 <phaugen@gmail.com>
2015-03-07 18:19:24 -0800

    place holder context set up

--- racts/spec/controllers/assignments_controller_spec.rb
+++ racts/spec/controllers/assignments_controller_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AssignmentsController, type: :controller do
-   feature 'Browsing assignments' do
-      describe "#complete" do
-        # it do
-        #   should route(:get, '/assignments/1').
-        #   to(action: :complete, id: 1)
-        # end
-      end
-   end
+
+  context "#complete" do
+  end
 end
