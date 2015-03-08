ad8fc2c2b98689273be4bc5c430265868af0f24c
wolfram74 <phaugen@gmail.com>
2015-03-07 17:21:39 -0800

    modifed #active_tasks to update subscriptions

--- racts/app/controllers/users_controller.rb
+++ racts/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 ####################
   def active_tasks  #/user/:id/active
     @user = User.find(params[:id])
+    @user.update_subscriptions
     @data = @user.active_tasks.to_json
     render json: @data
   end
