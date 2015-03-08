fef7663a0caa964de046245c1ff40305bc0f5b4c
Shambhavi Sachid <shambu1985@gmail.com>
2015-03-07 18:26:57 -0800

    Working on task completion angular

--- racts/public/ng/js/active.js
+++ racts/public/ng/js/active.js
@@ -1,5 +1,5 @@
 racts.factory('activeTasksModel', [function() {
-	
+
 	var data = {
 		assignments: []
 	}
@@ -26,35 +26,25 @@ racts.service('activeTasksResolver', ['$http', '$q', 'activeTasksModel', functio
 
 
 racts.service('activeTasksService', ['$http', '$q', 'activeTasksModel', function($http, $q, activeTasksModel ) {
-		
 
 		this.activeTasksModel = activeTasksModel
-		// getTasks();
 
-		// var q = $q.defer();
-
-		// save = function(task) {
-		// 	$http.post('http://localhost:3000/categoires' + currentCategory.current_category.id + '/tasks', {task: task})
-		// 	.success(function(response) {
-		// 		tasksList.push(response)
-		// 		q.resolve("Resolved!!")
-		// 	})
-		// 	.error(function(response) {
-		// 		q.reject("Rejected!")
-		// 	})
-		// }
-
-		// this.saveTask = function(task) {
-		// 	save(task);
-		// 	return q.promise
-		// }
-
-		// this.tasks = function() {
-		// 	return tasksList;
-		// }
 }])
 
+racts.service('activeTasksComplete', ['$http', '$q', 'activeTasksModel', 'activeTasksService', function($http, $scope, activeTasksModel, activeTasksService) {
+	var completedTask = function(task) {
+			if(confirm("Are you sure?")) {
+				$http.put("http://localhost:3000/assignments/" + #{task.assignemnt_id})
+				.success(function(response) {
+					console.log(response)
 
+				})
+				.error(function(response) {
+					console.log("Rejected")
+				})
+			}
+	}
+}])
 
 racts.controller('activeTasksController', ['$http', '$scope', 'activeTasksService', function($http, $scope, activeTasksService){
 
@@ -62,9 +52,12 @@ racts.controller('activeTasksController', ['$http', '$scope', 'activeTasksServic
 
 	console.log($scope.assignments)
 
-
-
+	// $scope.completeTask = activeTasksComplete
 }])
+
+
+
+
 
 
 
