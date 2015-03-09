ebf21a2ae8e7ddebabf1b4ae30753fda432d792d
Shambhavi Sachid <shambu1985@gmail.com>
2015-03-08 18:35:06 -0700

    Fixed unsubscribe action

--- racts/app/controllers/subscriptions_controller.rb
+++ racts/app/controllers/subscriptions_controller.rb
@@ -4,14 +4,14 @@ class SubscriptionsController < ApplicationController
     # @categories = @subscriptions.map{|subscription| Category.find(subscription.category_id)}
     # object = {}
     # object[:list] = @categories
-    render json: @subscriptions
+    render json: {list: @subscriptions}
   end
 
   def show
     @subscription = Subscription.find(params[:id])
     @category = Category.find(@subscription.category_id)
     @tasks = @category.tasks
-    object = @tasks 
+    object = @tasks
     render json: object
   end
 
