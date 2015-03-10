11ca4f194ee7f5e235005f5356867ed76ced6dbb
wolfram74 <phaugen@gmail.com>
2015-03-10 14:33:49 -0700

    post route implemented and tested for new tasks

--- racts/spec/controllers/tasks_controller_spec.rb
+++ racts/spec/controllers/tasks_controller_spec.rb
@@ -20,6 +20,16 @@ RSpec.describe TasksController, type: :controller do
       results.sort!
       expect(results.first).to_not eq(results.last)
     end
+
+    it "can hit post route" do
+      post "create", {}
+      expect(response.status).to eq(200)
+    end
+    it "can create with post route" do
+      expect{
+        post "create", {name: "%x" % rand(10**6), description:"a thing", cost_estimate: 5} 
+      }.to change{Task.count}
+    end
   end
 
 end
