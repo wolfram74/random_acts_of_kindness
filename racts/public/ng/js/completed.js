027445536acf246b8b76d814ad2a415ab5d5cf9a
Shambhavi Sachid <shambu1985@gmail.com>
2015-03-10 16:37:34 -0700

    Working on vote button

--- racts/public/ng/js/completed.js
+++ racts/public/ng/js/completed.js
@@ -32,23 +32,37 @@ racts.service('completedTasksResolver', ['$http', '$q', 'completedTasksModel', '
   console.log('********** CompletedTaskResolver initiated TEST END **********')
   return getCompletedTasks
 
-  // $scope.gottenTasks = $http.get('http://localhost:3000/users/'+session.currentUser().id+'/assignments')
-  //       .success(function(response) {
-  //         completedTasksModel.assignments = response
-  //         console.log('completedTasks after API call :')
-  //         console.log( completedTasksModel.assignments )
-  //       })
-  //       .error(function(response){
-  //         console.log('error with fetching completed tasks')
-  //       })
-
 }])
 
 
-racts.service('completedTasksService', ['$http', '$q', 'completedTasksModel', function($http, $q, completedTasksModel ) {
+racts.service('completedTasksService', ['$http', '$q', 'completedTasksModel','session', function($http, $q, completedTasksModel, session ) {
 
     this.completedTasksModel = completedTasksModel
 
+    this.like = function(task){
+      console.log("http://localhost:3000/users/" + session.currentUser().id + "/Task/" + task.assignment_id + '/vote?change=1')
+      $http.post("http://localhost:3000/users/" + session.currentUser().id + "/Task/" + task.assignment_id + '/vote?change=1')
+        .success(function(response) {
+          console.log(response)
+          console.log("DONE!")
+        })
+        .error(function(response) {
+          console.log("Rejected!")
+        })
+      }
+
+
+    this.unlike = function(task){
+      console.log("http://localhost:3000/users/" + session.currentUser().id + "/Task/" + task.assignment_id + '/vote?change=-1');
+      $http.post("http://localhost:3000/users/" + session.currentUser().id + "/Task/" + task.assignment_id + '/vote?change=-1')
+        .success(function(response) {
+          console.log(response)
+          console.log("DONE!")
+        })
+        .error(function(response) {
+          console.log("Rejected!")
+        })
+      }
 }])
 
 
@@ -56,13 +70,19 @@ racts.service('completedTasksService', ['$http', '$q', 'completedTasksModel', fu
 racts.controller('completedTasksController', ['$http', '$scope', 'completedTasksService', function($http, $scope, completedTasksService){
 
   $scope.assignments = completedTasksService.completedTasksModel.assignments
+  $scope.ucount=0;
+  $scope.lcount=0;
+  $scope.likeTask =  function(task, index) {
+
+         completedTasksService.like(task)
+
+  }
+
+  $scope.unlikeTask =  function(task, index) {
+
+         completedTasksService.unlike(task)
+
+
+  }
 }])
-
-
-
-
-
-
-
-
 
