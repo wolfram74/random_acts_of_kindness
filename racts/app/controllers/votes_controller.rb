e975d40c792aa7c1a8ee50f93e07af2a915f0738
wolfram74 <phaugen@gmail.com>
2015-03-10 17:45:07 -0700

    found bug in old patch

--- racts/app/controllers/votes_controller.rb
+++ racts/app/controllers/votes_controller.rb
@@ -8,12 +8,13 @@ class VotesController < ApplicationController
       sign = 1
     end
     history = User.find(params[:user_id]).votes.last(10)
-    magnitude = vote.magnitude_finder(history, sign)
     if old_vote
+      magnitude = old_vote.magnitude_finder(history, sign)
       old_vote.magnitude = magnitude
       old_vote.save
     else
       vote = Vote.new({user_id: params[:user_id], votable_id: params[:id], votable_type: params[:class_name]})
+      magnitude = vote.magnitude_finder(history, sign)
       vote.magnitude = magnitude
       vote.save
     end
